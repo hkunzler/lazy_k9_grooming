554be41810ecd1ddbb227a7f4981ed7fd8dfd76c
hkunzler <hkunzler@gmail.com>
2024-09-19 16:02:56 -0800

    build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,29 +8,24 @@ const config: Config = {
     theme: {
         extend: {
             colors: {
-                primary: '#7D53DE', // Add your brand's primary color
-                secondary: '#33157A', // Add a secondary color
-                accent: '#E2E2E2', // Add an accent color
-                neutral: '#333', // Neutral color for text and background
-                background: '#E2E2E2', // Custom background color
+                primary: '#7D53DE',
+                secondary: '#33157A',
+                background: '#E2E2E2',
             },
             fontFamily: {
-                sans: ['Inter', 'sans-serif'], // Custom font, fallback to sans-serif
-                heading: ['Poppins', 'sans-serif'], // Font for headings
+                sans: ['Inter', 'sans-serif'], 
+                heading: ['Poppins', 'sans-serif'],
             },
             spacing: {
-                '72': '18rem', // Add a custom large spacing size
+                '72': '18rem',
                 '84': '21rem',
                 '96': '24rem',
             },
             borderRadius: {
-                lg: '1.5rem', // Custom border radius
+                lg: '1.5rem', 
             },
             fontFamily: {
-                // Setting K2D as the default font
                 sans: ['K2D', 'sans-serif'],
-
-                // Custom font families for specific tags
                 ptSans: ['PT Sans', 'sans-serif'],
                 wendy: ['Wendy One', 'sans-serif'],
             },
