70026915b30738f9c542ce337e746542eea45fb9
hkunzler <hkunzler@gmail.com>
2024-09-20 15:12:16 -0800

    testing basepath for images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
+    basePath: '/lazy_k9_grooming',
 };
 
 export default nextConfig;
--- src/app/components/GalleryPreview.tsx
+++ src/app/components/GalleryPreview.tsx
@@ -15,14 +15,20 @@ export default function GalleryPreview() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const handlePrev = () => {
-        setCurrentIndex(currentIndex => currentIndex > 0 ? currentIndex - 1 : 0);
+        setCurrentIndex((currentIndex) =>
+            currentIndex > 0 ? currentIndex - 1 : 0,
+        );
     };
 
     const handleNext = () => {
-        setCurrentIndex(currentIndex => currentIndex < images.length - 1 ? currentIndex + 1 : currentIndex);
+        setCurrentIndex((currentIndex) =>
+            currentIndex < images.length - 1 ? currentIndex + 1 : currentIndex,
+        );
     };
 
-    const maxTransformIndex = images.length - 3;     const transformPercentage = Math.min(currentIndex, maxTransformIndex) * (100 / 3);
+    const maxTransformIndex = images.length - 3;
+    const transformPercentage =
+        Math.min(currentIndex, maxTransformIndex) * (100 / 3);
     return (
         <div className="relative w-full max-w-7xl mx-auto overflow-hidden">
             <div
