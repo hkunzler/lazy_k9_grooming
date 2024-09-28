7319800cc78911fc9e292f051e6fa22bdd3ba022
hkunzler <hkunzler@gmail.com>
2024-09-27 16:25:21 -0800

    image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image';
-import logo from '../assets/hero_logo.png';
+import background from '../assets/hero_background.svg';
+import logo from '../assets/logo.png';
 
 const Hero = () => {
     return (
         <section className="relative h-[50vh] ">
             <Image
-                src={logo}
+                src={background}
                 alt="Lazy K9 Grooming"
                 className="absolute top-[-30%] left-0 w-full h-[70vh] object-cover"
+            />
+	     <Image
+                src={logo}
+                alt="Lazy K9 Grooming"
+                className="absolute left-0 w-full h-[70vh] object-contain"
             />
         </section>
     );
