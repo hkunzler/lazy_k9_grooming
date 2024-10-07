2d15a00e7652f150b3fab2007d11c62fddd9a3d6
hkunzler <hkunzler@gmail.com>
2024-10-07 14:11:19 -0800

    render blocking fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,43 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+	  <head>
+                {/* Preload Fonts */}
+                <link
+                    rel="preload"
+                    href="https://fonts.googleapis.com/css2?family=K2D:wght@400;700&display=swap"
+                    as="style"
+                    onLoad="this.rel='stylesheet'"
+                />
+                <link
+                    rel="preload"
+                    href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+                    as="style"
+                    onLoad="this.rel='stylesheet'"
+                />
+                <link
+                    rel="preload"
+                    href="https://fonts.googleapis.com/css2?family=Wendy+One&display=swap"
+                    as="style"
+                    onLoad="this.rel='stylesheet'"
+                />
+                
+                {/* Fallback for browsers with no JS */}
+                <noscript>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=K2D:wght@400;700&display=swap"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Wendy+One&display=swap"
+                    />
+                </noscript>
+            </head>
             <body className="flex flex-col min-h-screen">
                 <Header />
                 <main className="flex-grow">{children}</main>
