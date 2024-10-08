73f1ba951b0c3fe01c0636c54b0769b0f901e98b
hkunzler <hkunzler@gmail.com>
2024-10-07 16:32:12 -0800

    fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,51 +2,37 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import './globals.css';
 
+import { K2D, PT_Sans, Wendy_One } from 'next/font/google';
+
+const k2d = K2D({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-k2d',
+});
+
+const ptSans = PT_Sans({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-pt-sans',
+});
+
+const wendyOne = Wendy_One({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: '--font-wendy-one',
+});
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <head>
-                {/* Preload Fonts */}
-                <link
-                    rel="preload"
-                    href="https://fonts.googleapis.com/css2?family=K2D:wght@400;700&display=swap"
-                    as="style"
-                    onLoad={(e) => (e.currentTarget.rel = 'stylesheet')}
-                />
-                <link
-                    rel="preload"
-                    href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
-                    as="style"
-                    onLoad={(e) => (e.currentTarget.rel = 'stylesheet')}
-                />
-                <link
-                    rel="preload"
-                    href="https://fonts.googleapis.com/css2?family=Wendy+One&display=swap"
-                    as="style"
-                    onLoad={(e) => (e.currentTarget.rel = 'stylesheet')}
-                />
-
-                {/* Fallback for browsers with no JS */}
-                <noscript>
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css2?family=K2D:wght@400;700&display=swap"
-                    />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
-                    />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css2?family=Wendy+One&display=swap"
-                    />
-                </noscript>
-            </head>
-            <body className="flex flex-col min-h-screen">
+        <html
+            lang="en"
+            className={`${k2d.variable} ${ptSans.variable} ${wendyOne.variable}`}
+        >
+            <body className="flex flex-col min-h-screen font-sans">
                 <Header />
                 <main className="flex-grow">{children}</main>
                 <Footer />
